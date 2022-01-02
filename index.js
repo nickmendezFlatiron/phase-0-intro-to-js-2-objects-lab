f33ae1239c7a2a3884b28fcbb1767f8b4544f9b3
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-01-02 18:24:19 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name : "nameValue",
+    streetAddress : "streetAddressValue",
+};
+
+function updateEmployeeWithKeyAndValue(object,key,value) {
+   return { ...object,
+    [key] : value } ;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object,key,value){
+    object [key] = value;
+    return object;
+}
+
+function deleteFromEmployeeByKey(object,key,value) {
+    const newEmployee = {...object};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object,key,value) {
+    delete object[key];
+    return object;
+}
